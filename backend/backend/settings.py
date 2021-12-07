280abb78a3fa28dbf3f4350df8b408e65f15b3df
TavaresDylan <tavares.dylan03@gmail.com>
2021-12-07 15:16:07 +0100

    feat(docker): dockerized app with database

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.environ.get('MYSQL_DATABASE'),
+        'USER': os.environ.get('MYSQL_USER'),
+        'PASSWORD': os.environ.get('MYSQL_PASSWORD'),
+        'HOST': 'db',
+        'PORT': 3306,
     }
 }
 
