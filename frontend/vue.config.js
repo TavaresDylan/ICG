3621e780240e9fe6900df5bffc6b570efa906ff1
TavaresDylan <tavares.dylan03@gmail.com>
2021-12-07 11:16:37 +0100

    fix(front): static outputDir path fixed, add watch

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 module.exports = {
     publicPath: '/static/src/vue/dist/', // Should be STATIC_URL + path/to/build
-    outputDir: path.resolve(__dirname, '../backend/static/src/vue/dist/'), // Output to a directory in STATICFILES_DIRS
+    outputDir: path.resolve(__dirname, '../backend/static/src/vue/dist'), // Output to a directory in STATICFILES_DIRS
     filenameHashing: false, // Django will hash file names, not webpack
     runtimeCompiler: true, // See: https://vuejs.org/v2/guide/installation.html#Runtime-Compiler-vs-Runtime-only
     devServer: {
