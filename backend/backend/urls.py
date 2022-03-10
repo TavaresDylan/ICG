4ec0a075aa9e916340fe2c4522ad5c58b69f4d78
TavaresDylan <tavares.dylan03@gmail.com>
2022-03-10 19:10:35 +0100

    feat(back): add media url

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, re_path, include
 from rest_framework import routers
-
+from django.conf.urls.static import static
 from api.urls import router as api_router
+from django.conf import settings
 
 router = routers.DefaultRouter()
 router.registry.extend(api_router.registry)
@@ -26,5 +27,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     # API
     re_path(r'^', include('api.urls')),
-	path('', include(router.urls))
-]
+	path('', include(router.urls)),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
