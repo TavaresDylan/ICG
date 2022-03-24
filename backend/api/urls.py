f1ae1501b100a0ec4982ae864910e10885337614
TavaresDylan <tavares.dylan03@gmail.com>
2022-03-24 20:30:06 +0100

    feat(back): add folder and demo route & replace auth by jwt

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,13 +1,15 @@
 from django.urls import path, include
 from rest_framework import routers
-from api.views import RegisterViewset, UploadViewset
+from api.views import RegisterViewset, UploadViewset, DemoViewset, FolderViewset
 
 router = routers.SimpleRouter()
 router.register('register', RegisterViewset, basename='register')
 router.register('upload', UploadViewset , basename='upload')
+router.register('demo', DemoViewset, basename='demo')
+router.register('folder', FolderViewset, basename='folder')
 
 urlpatterns = [
     path('api/v1/', include(router.urls)),
     path('api/v1/', include('djoser.urls')),
-    path('api/v1/', include('djoser.urls.authtoken')),
+    path('api/v1/', include('djoser.urls.jwt')),
 ]
