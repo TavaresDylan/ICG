3321917eeeeb8e5663cdeceecd623538465ea2fa
TavaresDylan <tavares.dylan03@gmail.com>
2022-04-24 21:39:29 +0200

    feat(front): handle alert message

--- frontend/src/views/Login.vue
+++ frontend/src/views/Login.vue
@@ -1,23 +1,14 @@
 <template>
-  <v-main>
+  <v-main class="d-flex align-center">
     <h1 class="text-center mt-12">
       <v-icon color="black">mdi-login</v-icon> Log In
     </h1>
     <v-container class="pa-12">
-      <v-alert
-        :value="errorLoginStatus"
-        dense
-        border="left"
-        type="error"
-        dismissible
-        transition="scroll-y-reverse-transition"
-        >{{ errorLoginMsg }}</v-alert
-      >
       <v-row justify="center">
         <v-col cols="12" class="col-sm-8 col-md-6 col-lg-4">
           <v-form ref="form" v-model="valid" lazy-validation>
             <v-text-field
-              @focus="handleError()"
+              @focus="resetAlert()"
               v-model="creds.username"
               :rules="[rules.required]"
               label="Name"
@@ -27,6 +18,7 @@
             <v-text-field
               @keydown.enter.prevent="login"
               @click:append="show1 = !show1"
+              @focus="resetAlert()"
               :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
               :type="show1 ? 'text' : 'password'"
               v-model="creds.password"
@@ -51,38 +43,28 @@
 </template>
 
 <script>
-import { mapActions, mapState, mapMutations } from "vuex";
+import { mapActions, mapMutations } from "vuex";
 export default {
   data: () => {
     return {
-      username: "",
-      password: "",
       creds: {
         name: "",
         password: "",
       },
       show1: false,
-      loading: false,
-      valid: true,
+      valid: false,
       rules: {
         required: (value) => !!value || "Requis.",
       },
     };
   },
-  computed: {
-    ...mapState("auth", ["errorLoginMsg", "errorLoginStatus"]),
-  },
   methods: {
     ...mapActions("auth", ["logUser"]),
-    ...mapMutations("auth", ["resetError"]),
+    ...mapMutations(["resetAlert"]),
     login() {
       if (this.$refs.form.validate()) {
-        this.loading = true;
         this.logUser(this.creds);
       }
-    },
-    handleError() {
-      this.resetError();
     },
   },
 };
