661d9c8a53119283122719bad28bc704b1205d6a
TavaresDylan <tavares.dylan03@gmail.com>
2022-05-02 17:02:12 +0200

    refactor(front): page not found new svg image

--- frontend/src/views/PageNotFound.vue
+++ frontend/src/views/PageNotFound.vue
@@ -1,11 +1,14 @@
 <template>
-  <v-container>
-    <v-row justify="center">
-      <v-col cols="10" class="col-xs-8 col-md-10 col-lg-7 col-xl-4">
-        <v-img contain :src="require('../assets/404.png')"></v-img>
-      </v-col>
-    </v-row>
-  </v-container>
+  <v-main>
+    <v-container fill-height>
+      <v-row justify="center">
+        <v-col cols="10" class="col-xs-8 col-md-10 col-lg-7 col-xl-4">
+          <v-img contain max-height="40vh" :src="require('../assets/taken.svg')"></v-img>
+          <p class="text-center pa-6 display-1">Ooops ! You this page seems to has been kidnapped by some unknown creatures.</p>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-main>
 </template>
 
 <script>
