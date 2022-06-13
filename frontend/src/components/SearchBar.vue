324f247bcbff32cf1520b40a4d358947072f0d56
TavaresDylan <tavares.dylan03@gmail.com>
2022-06-13 04:30:17 +0200

    feat(front): search bar component redesigned

--- frontend/src/components/SearchBar.vue
+++ frontend/src/components/SearchBar.vue
@@ -1,40 +1,12 @@
 <template>
-  <v-autocomplete
-    @change="getBySearch"
-    v-model="model"
-    :items="items"
-    :loading="isLoading"
-    :search-input.sync="search"
-    chips
-    clearable
-    hide-details
-    hide-selected
-    item-text="name"
-    item-value="symbol"
-    :label="searchLabel"
-    solo
-  >
-    <template v-slot:no-data>
-      <v-list-item>
-        <v-list-item-title>
-          Search by image name or by
-          <strong>description</strong>
-        </v-list-item-title>
-      </v-list-item>
-    </template>
-    <template v-slot:item="{ item }">
-      <v-list-item-avatar
-        color="indigo"
-        class="text-h5 font-weight-light white--text"
-      >
-        {{ item.name.charAt(0) }}
-      </v-list-item-avatar>
-      <v-list-item-content>
-        <v-list-item-title v-text="item.name"></v-list-item-title>
-        <v-list-item-subtitle v-text="item.symbol"></v-list-item-subtitle>
-      </v-list-item-content>
-    </template>
-  </v-autocomplete>
+  <v-form @submit.prevent class="d-flex align-center justify-center">
+    <v-text-field
+      :placeholder="searchLabel"
+      v-model="search"
+      append-icon="mdi-magnify"
+    >
+    </v-text-field>
+  </v-form>
 </template>
 
 <script>
