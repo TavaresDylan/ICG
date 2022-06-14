265c8d6f970672f597764fa76bcd2801ba46818f
TavaresDylan <tavares.dylan03@gmail.com>
2022-06-14 02:03:51 +0200

    feat(back): remove useless register serializer and add some user fields

--- backend/api/serializer.py
+++ backend/api/serializer.py
@@ -1,40 +1,11 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
-from rest_framework.validators import UniqueValidator
 from api.models import Photo, Demo, Folder
-
-class RegisterSerializer(serializers.ModelSerializer):
-    email = serializers.EmailField(
-        required=True, validators=[UniqueValidator(queryset=User.objects.all())]
-    )
-
-    password = serializers.CharField(write_only=True, required=True)
-
-    class Meta:
-        model = User
-        fields = ("username", "password", "email")
-
-    def create(self, validated_data):
-        user = User.objects.create_user(
-            username=validated_data["username"],
-            email=validated_data["email"],
-        )
-
-        user.set_password(validated_data["password"])
-        user.save()
-
-        return user
-
 
 class CurrentUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ("id", "username", "date_joined", "email")
-
-    def get(self):
-        user = User.objects.filter(id=self.get(id))
-
-        return user
+        fields = ('id', 'username', 'date_joined', 'email', 'is_superuser', 'first_name', 'last_name', 'is_staff')
 
 class PhotoSerializer(serializers.ModelSerializer):
 	class Meta:
